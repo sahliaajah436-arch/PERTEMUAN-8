557fe03bc8b040ec4ec161a17d65508404621376
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-04-23 11:21:53 +0700

    menampilkan " Duplikasi noMhs tidak diijinkan"

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -21,7 +21,23 @@ public:
 
     void addMode()
     {
-        int main;
+        int nim;
         cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if(START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+
+            nodeBaru->next = START;
+            
+        }
     }
 };
