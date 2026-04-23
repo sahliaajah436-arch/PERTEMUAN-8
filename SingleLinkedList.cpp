41e1f5f28d255f4273e43f14241b77144ae25347
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-04-23 11:37:49 +0700

    menambahkan fungsi list empty dan search

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -46,7 +46,36 @@ public:
 
         while (current != NULL && nim > current->noMhs)
         {
-            
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+
         }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+
+    bool listEmpty()
+    {
+        return (START == NULL);
+    }
+
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
     }
 };
