6857276dd4d0c6f9ad2712bd0ffc3c8ae1ad962a
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-04-23 11:26:54 +0700

    memasukkan printah while

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -43,5 +43,10 @@ public:
 
         Node *previous = START;
         Node *current = START;
+
+        while (current != NULL && nim > current->noMhs)
+        {
+            
+        }
     }
 };
